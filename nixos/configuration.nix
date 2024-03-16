f9337c30dddbf5aceb164de54eb9b86a6e023190
a2not <fb503d52.af1c4d@gmail.com>
2024-03-16 11:55:07 +0900

    merged hardware-config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -40,8 +40,25 @@
   networking.hostName = systemSettings.hostname;
   networking.networkmanager.enable = true;
 
-  # TODO: systemd-boot cannot be enabled safely with lima VM
-  # boot.loader.systemd-boot.enable = true;
+  # NOTE: for lima: system mounts
+  boot.loader.grub = {
+    device = "nodev";
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
+  fileSystems."/boot" = {
+    device = lib.mkDefault "/dev/vda1"; # /dev/disk/by-label/ESP
+    fsType = "vfat";
+  };
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    autoResize = true;
+    fsType = "ext4";
+    options = ["noatime" "nodiratime" "discard"];
+  };
+  # NOTE: for lima: system mounts
+
+  nixpkgs.hostPlatform = systemSettings.system;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   time.timeZone = systemSettings.timezone;
--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -1,24 +1,2 @@
-{
-  lib,
-  systemSettings,
-  ...
-}: {
-  # NOTE: for lima: system mounts
-  boot.loader.grub = {
-    device = "nodev";
-    efiSupport = true;
-    efiInstallAsRemovable = true;
-  };
-  fileSystems."/boot" = {
-    device = lib.mkDefault "/dev/vda1"; # /dev/disk/by-label/ESP
-    fsType = "vfat";
-  };
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    autoResize = true;
-    fsType = "ext4";
-    options = ["noatime" "nodiratime" "discard"];
-  };
-
-  nixpkgs.hostPlatform = systemSettings.system;
+{...}: {
 }
